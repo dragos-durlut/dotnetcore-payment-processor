d96fbd4a5e341ab5ed706dc7d0b48a98fc4ccc8d
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-02 00:16:47 +0200

    - added migration

--- PaymentProcessor.Models.Entity/Migrations/20200201221526_PaymentState_CreatedDate.Designer.cs
+++ PaymentProcessor.Models.Entity/Migrations/20200201221526_PaymentState_CreatedDate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PaymentProcessor.Models.Entity;
 
 namespace PaymentProcessor.Models.Entity.Migrations
 {
     [DbContext(typeof(PaymentProcessorContext))]
-    partial class PaymentProcessorContextModelSnapshot : ModelSnapshot
+    [Migration("20200201221526_PaymentState_CreatedDate")]
+    partial class PaymentState_CreatedDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
