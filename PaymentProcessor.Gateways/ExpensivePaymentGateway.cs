bada94c46f25f5c6dfd3ba5ddf12a0f01985d729
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-02 00:59:57 +0200

    - implemented failure mechanism

--- PaymentProcessor.Gateways/ExpensivePaymentGateway.cs
+++ PaymentProcessor.Gateways/ExpensivePaymentGateway.cs
@@ -9,7 +9,13 @@ namespace PaymentProcessor.Gateways
     {
         public PaymentStateDto ProcessPayment(PaymentRequestDto paymentRequest)
         {
-            return new PaymentStateDto() { PaymentState = PaymentStateEnum.Failed, PaymentStateDate = DateTime.UtcNow };
+            Random rnd = new Random();
+            var num = rnd.Next(1, 12);
+            if (num % 4 == 0 || num % 6 == 0)
+                throw new Exception("Call failed");
+            if (num % 2 == 0)
+                return new PaymentStateDto() { PaymentState = PaymentStateEnum.Failed, PaymentStateDate = DateTime.UtcNow };
+            else return new PaymentStateDto() { PaymentState = PaymentStateEnum.Processed, PaymentStateDate = DateTime.UtcNow };
         }
     }
 }
